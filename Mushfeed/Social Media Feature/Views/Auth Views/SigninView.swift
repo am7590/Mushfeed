1a6b4bc04b3ede5a88de52583ddf6dea703a9b31
Alek Michelson <am7590@rit.edu>
2021-09-11 15:13:59 -0400

    Got sign in with apple account creation working in SignupView

--- Mushfeed/Social Media Feature/Views/Auth Views/SigninView.swift
+++ Mushfeed/Social Media Feature/Views/Auth Views/SigninView.swift
@@ -8,45 +8,62 @@
                     
 import SwiftUI
 import AuthenticationServices
+import CryptoKit
+import FirebaseAuth
 
 struct SigninView: View {
     
     @AppStorage("email") var storageEmail: String = ""
     @AppStorage("username") var storageUsername: String = ""
     @AppStorage("userId") var storageUserId: String = ""
-    
-
-    
     @ObservedObject var signinViewModel = SigninViewModel()
     @Environment(\.colorScheme) var colorScheme
     
     func configure(_ request: ASAuthorizationAppleIDRequest){
-        request.requestedScopes = [.email, .fullName]
+        let nonce = randomNonceString()
+        currentNonce = nonce
+        request.requestedScopes = [.fullName, .email]
+        request.nonce = sha256(nonce)
     }
     
     
     func handle(_ authResult: Result<ASAuthorization, Error>){
         switch authResult {
-        case .success(let auth):
-            print(auth)
-            switch auth.credential {
-            case let credential as ASAuthorizationAppleIDCredential:
-                let userId = credential.user
-                let email = credential.email
-                let username = credential.fullName?.givenName
-                //let lastname = credential.fullName?.familyName
-                self.storageEmail = email ?? ""
-                self.storageUserId = userId
-                self.storageUsername = username ?? ""
-                
-                break
-            default:
-                break
-            }
-            break
-        case .failure(let error):
-            print(error)
-        }
+                                      case .success(let authResults):
+                                          switch authResults.credential {
+                                              case let appleIDCredential as ASAuthorizationAppleIDCredential:
+                                              
+                                                      guard let nonce = currentNonce else {
+                                                        fatalError("Invalid state: A login callback was received, but no login request was sent.")
+                                                      }
+                                                      guard let appleIDToken = appleIDCredential.identityToken else {
+                                                          fatalError("Invalid state: A login callback was received, but no login request was sent.")
+                                                      }
+                                                      guard let idTokenString = String(data: appleIDToken, encoding: .utf8) else {
+                                                        print("Unable to serialize token string from data: \(appleIDToken.debugDescription)")
+                                                        return
+                                                      }
+                                                     
+                                                      let credential = OAuthProvider.credential(withProviderID: "apple.com",idToken: idTokenString,rawNonce: nonce)
+                                                      Auth.auth().signIn(with: credential) { (authResult, error) in
+                                                          if (error != nil) {
+                                                              // Error. If error.code == .MissingOrInvalidNonce, make sure
+                                                              // you're sending the SHA256-hashed nonce as a hex string with
+                                                              // your request to Apple.
+                                                              print(error?.localizedDescription as Any)
+                                                              return
+                                                          }
+                                                          print("signed in")
+                                                      }
+                                              
+                                                  print("\(String(describing: Auth.auth().currentUser?.uid))")
+                                          default:
+                                              break
+                                                      
+                                                  }
+                                         default:
+                                              break
+                                      }
     }
     
     
@@ -69,6 +86,53 @@ struct SigninView: View {
         self.signinViewModel.password = ""
     }
     
+    @State var currentNonce:String?
+        
+        //Hashing function using CryptoKit
+        func sha256(_ input: String) -> String {
+            let inputData = Data(input.utf8)
+            let hashedData = SHA256.hash(data: inputData)
+            let hashString = hashedData.compactMap {
+            return String(format: "%02x", $0)
+            }.joined()
+
+            return hashString
+        }
+    
+    // from https://firebase.google.com/docs/auth/ios/apple
+    private func randomNonceString(length: Int = 32) -> String {
+      precondition(length > 0)
+      let charset: Array<Character> =
+          Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+      var result = ""
+      var remainingLength = length
+
+      while remainingLength > 0 {
+        let randoms: [UInt8] = (0 ..< 16).map { _ in
+          var random: UInt8 = 0
+          let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
+          if errorCode != errSecSuccess {
+            fatalError("Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)")
+          }
+          return random
+        }
+
+        randoms.forEach { random in
+          if remainingLength == 0 {
+            return
+          }
+
+          if random < charset.count {
+            result.append(charset[Int(random)])
+            remainingLength -= 1
+          }
+        }
+      }
+
+      return result
+    }
+    
+
     @State var email: String = ""
     @State var password: String = ""
     
